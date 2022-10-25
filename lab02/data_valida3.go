9f361ad9344349900ce509d5c98d00b2be6cd022
giuseppe_difilippo <giuseppe.difilippo@live.it>
2022-10-25 10:25:22 +0200

    funzione ridotta

--- lab02/data_valida3.go
+++ lab02/data_valida3.go
@@ -1,16 +1,7 @@
 package main
 import "fmt"
 func isBi(y int) bool {
-  if y % 4 == 0 {
-    if y%100 != 0 || y%400 == 0 {
-      return true
-    } else {
-      return false
-    }
-  } else {
-    return false
-  }
-  }
+    return y%4==0 && (y%100!=0 || y%400==0)
   func main() {
     var gg,mm ,aa int
     mesi:=[]int{1,31,2,28,3,31,4,30,5,31,6,30,7,31,8,31,9,30,10,31,11,30,12,31}
