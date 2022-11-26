8cc772162a3dc0570a76808ce01dec427b183bcf
giuseppe_difilippo <giuseppe.difilippo@live.it>
2022-11-26 09:00:02 +0100

    finito quadrati circa

--- lab07/Tema/quadrati/quadrati.go
+++ lab07/Tema/quadrati/quadrati.go
@@ -1,16 +1,26 @@
 package main
 import (
       "fmt"
-      //"os"
+      "os"
       "math"
+      "strconv"
       )
 
 func isSquare(n int ) bool {
-  return math.Sqrt(float64(n))/1 == 0.0
+x:= math.Sqrt(float64(n))
+str := fmt.Sprintf("%2.2f", x )
+a, _:= strconv.Atoi(str[len(str)-2:])
+return a == 0
 }
 func main() {
-  //arr := os.Args[1:]
-  var x int
-  fmt.Scan(&x)
-  fmt.Println(isSquare(x))
-}
+  var vec []int
+  vec = make([]int, 0)
+  arr := os.Args[1:]
+  for i:=0 ; i<len(arr); i++ {
+    x, err := strconv.Atoi(arr[i])
+    if err == nil && isSquare(x){
+    vec = append(vec, x)
+  }
+  }
+  fmt.Println(vec)
+}// funziona alla carta ma fallisce il test, probabile che il programma di test voglia un tipo di output ben specifico ma dal comando non riesco a capirlo  
