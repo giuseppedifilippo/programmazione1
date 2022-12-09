eeaf85226de87e7cb923bf7395837543041e2b13
giuseppe_difilippo <giuseppe.difilippo@live.it>
2022-12-09 17:36:38 +0100

    added posizioni

--- Lab08/posizioni_parole.go
+++ Lab08/posizioni_parole.go
@@ -2,11 +2,40 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strings"
 )
+
+//rimuovere duplicati
+func rmDup(x []string) []string {
+	keys := make(map[string]bool)
+	list := []string{}
+	for _, el := range x {
+		if _ , value := keys[el]; !value {
+			keys[el] = true
+			list = append(list ,el)
+		}
+	}	
+	return list 
+}
+
 func main() {
+	mappa := make(map[string][]int)
+	sl := make([]string, 0)
 	scanner:= bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		
+		a := strings.Split(scanner.Text(), " ")
+		sl = append(sl, a...)
 	}
+	check := rmDup(sl)
+	for _, key := range check {
+		mappa[key] = []int{}
+		for i, el := range sl {
+			if key == el {
+				mappa[key] = append(mappa[key], i )
+			}
+		}
+	}
+	fmt.Println(mappa)
 }
