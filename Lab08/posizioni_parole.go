35b8a71a787a09969058ad7a13b2d8a1c79a5bb8
giuseppe_difilippo <giuseppe.difilippo@live.it>
2022-12-11 18:31:54 +0100

    fixed posizioni

--- Lab08/posizioni_parole.go
+++ Lab08/posizioni_parole.go
@@ -25,7 +25,10 @@ func main() {
 	sl := make([]string, 0)
 	scanner:= bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		a := strings.Split(scanner.Text(), " ")
+		a := strings.Split(strings.Trim(scanner.Text()," "), " ")// questo trim elimina eventuali spazi a inizio o fine riga 
+		for _, el := range a { // la funzione di questo ciclo è di rimuovere eventuali spazi aggiuntivi
+			el = strings.Trim(el, " ") // tra una parola e l altra
+		}
 		sl = append(sl, a...)
 	}
 	check := rmDup(sl)
@@ -38,4 +41,7 @@ func main() {
 		}
 	}
 	fmt.Println(mappa)
-}
+}// il programma funziona ma ha un bug per cui se alla fine della riga inserisco uno spazio prima di inseire 
+// ENTER il programma conta anche lo spazio come parola(problema dovuto al funzionamento di strings.Split)
+// risolto
+// p.s so che mancano gli accenti nei commenti ma sto usando una tastiera inglese 
