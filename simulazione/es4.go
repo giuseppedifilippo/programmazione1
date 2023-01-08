1cadadcc2b10c6cfdc0c1c4dfec9538d7e68ff62
Giuseppe Difilippo <98238828+giuseppedifilippo@users.noreply.github.com>
2023-01-08 16:48:05 +0000

    commentato es2

--- simulazione/es4.go
+++ simulazione/es4.go
@@ -12,25 +12,28 @@ func main() {
 	d, _ := strconv.Atoi(os.Args[2])
 	strsl := strings.Split(n, "")
 	sl := []int{}
-	//creo una slice che contiene le cifre di n in tipp intero
+	//creo una slice che contiene le cifre di n in tipo intero
 	for _, el := range strsl {
 		x ,_ := strconv.Atoi(el)
 		sl = append(sl, x)
 	}
-
+  //itera d volte lungo la slice di interi 
 	for i:=0; i< d ; i++ {
 		var num, index int  
+    //questo ciclo trova il valore piu alto tra gli elementi della slice e ne memorizza il suo indice in index 
 		for j , el := range sl {
 			if el > num {
 				num, index  = el ,j
 			}
 		}
+    //queste tre righe successive eliminano dalla slice l elemento di indice index 
 		copy(sl[index:],sl[index+1:])
 		sl[len(sl)-1] = 0
 		sl = sl[:len(sl)-1]
 		
 	}
   out := ""
+  //unisco gli 
   for _, el := range sl {
     out += strconv.Itoa(el)
   }
