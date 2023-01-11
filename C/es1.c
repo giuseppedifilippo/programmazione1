a8ea86f9a64375ef2c4c35f9e1449920a5267c47
giuseppedifilippo <98238828+giuseppedifilippo@users.noreply.github.com>
2023-01-11 10:16:28 +0100

    aggiunto c

---
+++ C/es1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int contiene_caratteri_ripetuti(char *x) {
+    int n ,i, j;
+
+    n = strlen(x);
+    for (i=0;i<n;i++) 
+        for (j=i+1; j<n;i++)
+            if (x[i])==
+}
